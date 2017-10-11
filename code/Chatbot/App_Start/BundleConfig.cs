035825447545ee60e8382fe163f2fad9b5dd6b64
Felix Bauer <if16b002@technikum-wien.at>
2017-10-11 14:29:52 +0200

    Removed more wildcards

--- code/Chatbot/App_Start/BundleConfig.cs
+++ code/Chatbot/App_Start/BundleConfig.cs
@@ -14,7 +14,7 @@ namespace Chatbot
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.6.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
