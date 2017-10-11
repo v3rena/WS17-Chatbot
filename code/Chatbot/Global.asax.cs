48557c0e79686b94d546bebeb931739b6f197842
Felix Bauer <if16b002@technikum-wien.at>
2017-10-11 14:15:08 +0200

    Remove FilterConfig and BundleConfig

--- code/Chatbot/Global.asax.cs
+++ code/Chatbot/Global.asax.cs
@@ -13,11 +13,11 @@ namespace Chatbot
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            AreaRegistration.RegisterAllAreas();
+//            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+//            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
