d9c11798c2757d384f3b51910b6d11e8cdf4583c
Monika Mochty <if15b022@technikum-wien.at>
2017-10-27 21:06:06 +0200

    Added generic DAL files

---
+++ code/Chatbot.DataAccessLayer/BusinessLayer.cs
@@ -0,0 +1,33 @@
+﻿using Chatbot.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatbot.Services
+{
+    public class BusinessLayer<T> : IBusinessLayer<T>
+    {
+        private IDataAccessLayer<T> _dal;
+
+        public BusinessLayer(IDataAccessLayer<T> dal)
+        {
+            _dal = dal;
+        }
+
+        public string GetName()
+        {
+            return _dal.GetName();
+        }
+
+        public string GetTest(int id)
+        {
+            return _dal.GetTest(id);
+        }
+
+        public T SelectFirst(Func<T, bool> condition)
+        {
+            return _dal.SelectFirst(condition);
+        }
+    }
+}
---
+++ code/Chatbot.DataAccessLayer/DAL.cs
@@ -0,0 +1,80 @@
+﻿using Autofac;
+using Chatbot.Data.Setup;
+using Chatbot.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Chatbot.Data.DataAccessLayer
+{
+    public class DAL : IDataAccessLayer<Test>
+    {
+        public DAL()
+        {
+            Database.SetInitializer<TestContext>(new MigrateDatabaseToLatestVersion<TestContext, Migrations.Configuration>());
+        }
+
+        public string GetName()
+        {
+            return "DataAccessLayer";
+        }
+
+        public string GetTest(int id)
+        {
+            Test result;
+            using (var context = new TestContext())
+            {
+                result = context.Test.FirstOrDefault();
+            }
+            return result.TestName;
+        }
+
+        public int Insert(Test item)
+        {
+            using (var context = new TestContext())
+            {
+                context.Test.Add(item);
+                return context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Test> Select(Func<Test, bool> condition)
+        {
+            IEnumerable<Test> result = null;
+            using (var context = new TestContext())
+            {
+                result = context
+                    .Test
+                    .Where(condition)
+                    .ToList();
+            }
+            return result;
+        }
+
+        public Test SelectFirst(Func<Test, bool> condition)
+        {
+            return Select(condition).FirstOrDefault();
+        }
+
+        public class Module : Autofac.Module
+        {
+            protected override void Load(ContainerBuilder builder)
+            {
+                //base.Load(builder);
+                builder
+                    .Register(component => new DAL())
+                    .As<IDataAccessLayer<Test>>()
+                    .InstancePerLifetimeScope();
+
+                /*
+                 builder.Register(c => new ValuesService(c.Resolve<ILogger<ValuesService>>()))
+                        .As<IValuesService>()
+                    .InstancePerLifetimeScope();
+                 */
+            }
+        }
+
+    }
+}
