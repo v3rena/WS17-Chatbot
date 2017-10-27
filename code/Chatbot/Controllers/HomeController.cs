3c1ef04ad3f333e4fedda4afa6dcc68cfbaf8cb5
Felix Bauer <if16b002@technikum-wien.at>
2017-10-27 19:19:52 +0200

    #2, #14 Set Base path so it runs in virtual subdirectories

--- code/Chatbot/Controllers/HomeController.cs
+++ code/Chatbot/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Chatbot.Interfaces;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Chatbot.Controllers
@@ -12,6 +13,10 @@ namespace Chatbot.Controllers
 
         public ActionResult Index()
         {
+            var request = HttpContext.Request;
+            var baseUrl = string.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Authority, HttpRuntime.AppDomainAppVirtualPath == "/" ? "" : HttpRuntime.AppDomainAppVirtualPath);
+            ViewBag.BasePath = baseUrl;
+
             ViewBag.Title = "ChatBot";
 
             return View();
