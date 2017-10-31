fb2e45ef8bc42e5ea3079d13a2ffdee0aa5e7cb1
Harald Amon <if15b001@technikum-wien.at>
2017-10-31 12:43:49 +0100

    #5 created User for OpenWeatherMap; OpenWeatherMapAPIKey added

--- code/Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
+++ code/Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
@@ -2,6 +2,7 @@
 using Chatbot.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,22 @@ namespace Chatbot.Plugins.WeatherPlugin
     {
         public string Name => "WeatherPlugin";
 
+        private static List<string> stringLibrary = new List<string> { "wetter", "temperatur", "regen", "sonne", "wolken" };
+        public string Location { get; set; }
+
         public float CanHandle(Message message)
         {
-            throw new NotImplementedException();
+            message.Content.ToLower();
+
+            if (stringLibrary.Any(s => message.Content.Contains(s)))
+                return 1;
+            else
+                return 0;   
         }
 
         public Message Handle(Message message)
         {
+            //string apiKey = ConfigurationManager.AppSettings["OpenWeatherMapAPIKey"];
             throw new NotImplementedException();
         }
     }
