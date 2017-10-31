880235ec29ecc9f86cb429563b4568f26036ba9c
Harald Amon <if15b001@technikum-wien.at>
2017-10-31 12:25:12 +0100

    #5 WeatherPlugin init
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue Oct 31 12:25:12 2017 +0100
    #
    # interactive rebase in progress; onto a0437fc
    # Last command done (1 command done):
    #    edit bc28902 WeatherPlugin init
    # Next commands to do (2 remaining commands):
    #    edit 03da6e2 created User for OpenWeatherMap; OpenWeatherMapAPIKey added
    #    edit 0d6f6d6 #5 Created HttpClient: getWeatherInformation
    # You are currently editing a commit while rebasing branch '5-wetter-plugin' on 'a0437fc'.
    #
    # Changes to be committed:
    #	new file:   Chatbot.Plugins.WeatherPlugin/Chatbot.Plugins.WeatherPlugin.csproj
    #	new file:   Chatbot.Plugins.WeatherPlugin/Properties/AssemblyInfo.cs
    #	new file:   Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
    #

---
+++ code/Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Chatbot.Interfaces;
+using Chatbot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.Plugins.WeatherPlugin
+{
+    public class WeatherPlugin : IPlugin
+    {
+        public string Name => "WeatherPlugin";
+
+        public float CanHandle(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message Handle(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
