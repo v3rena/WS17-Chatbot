3da2fbe194c3a16774325fc9642ccd1908896473
Michael Palata <if15b037@technikum-wien.at>
2017-11-07 12:48:42 +0100

    added session controller and javascript to get guid

---
+++ code/Chatbot/Controllers/SessionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Chatbot.Controllers
+{
+    [RoutePrefix("api/session")]
+    public class SessionController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(
+                    String.Format("{{ guid : \"{0}\" }}", Guid.NewGuid().ToString()),
+                    Encoding.UTF8,
+                    "application/json"
+                )
+            };
+        }
+    }
+}
--- code/Chatbot/Scripts/messenger.js
+++ code/Chatbot/Scripts/messenger.js
@@ -1,4 +1,5 @@
 var url = "/api/message/";
+var guid;
 
 $("#messageForm").submit(function (event) {
     event.preventDefault();
@@ -44,4 +45,30 @@ function addMessage(message, error, me = null) {
     }
 
     $("#messageContainer").append("<span class=\"message " + poster + " " + status + "\">" + message.Content + "</span>");
+}
+
+$(document).ready(ensureSession);
+
+function ensureSession() {
+    console.log("hello");
+    if (typeof (Storage) !== "undefined") {
+        var _guid = localStorage.getItem("guid");
+        if (_guid === "undefined" || _guid == null) {
+            $.getJSON("api/session", function (json) {
+                console.log("s");
+                if (json.guid !== "undefined") {
+                    guid = json.guid;
+                    localStorage.setItem("guid", guid);
+                    console.log(guid);
+                } else {
+                    alert("Es konnte keine Session gestartet werden!");
+                }
+            });
+        } else {
+            guid = _guid;
+            console.log(guid);
+        }
+    } else {
+        alert("Sorry! No Web Storage support ...");
+    }
 }
