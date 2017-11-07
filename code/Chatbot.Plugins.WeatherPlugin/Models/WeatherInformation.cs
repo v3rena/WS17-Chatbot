c61451db92f4eb19203509aa71c723e1a9da44be
Harald Amon <if15b001@technikum-wien.at>
2017-11-07 11:55:49 +0100

    #5 Created HttpClient: getWeatherInformation

---
+++ code/Chatbot.Plugins.WeatherPlugin/Models/WeatherInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.Plugins.WeatherPlugin.Models
+{
+    class WeatherInformation
+    {
+        public string City { get; set; }
+        public decimal Temperature { get; set; }
+
+    }
+}
--- code/Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
+++ code/Chatbot.Plugins.WeatherPlugin/WeatherPlugin.cs
@@ -1,10 +1,12 @@
 ﻿using Chatbot.Interfaces;
 using Chatbot.Models;
+using Chatbot.Plugins.WeatherPlugin.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Chatbot.Plugins.WeatherPlugin
@@ -15,6 +17,14 @@ namespace Chatbot.Plugins.WeatherPlugin
 
         private static List<string> stringLibrary = new List<string> { "wetter", "temperatur", "regen", "sonne", "wolken" };
         public string Location { get; set; }
+        public string ApiKey { get; }
+
+        static HttpClient client = new HttpClient();
+
+        public WeatherPlugin()
+        {
+            ApiKey = ConfigurationManager.AppSettings["OpenWeatherMapAPIKey"];
+        }
 
         public float CanHandle(Message message)
         {
@@ -28,8 +38,42 @@ namespace Chatbot.Plugins.WeatherPlugin
 
         public Message Handle(Message message)
         {
-            //string apiKey = ConfigurationManager.AppSettings["OpenWeatherMapAPIKey"];
-            throw new NotImplementedException();
+            // api.openweathermap.org/data/2.5/weather?q=London
+
+            RunAsync().Wait();
+
+            return null;
+        }
+
+        async Task RunAsync()
+        {
+            client.BaseAddress = new Uri($"api.openweathermap.org/data/2.5/forecast?q=London?id={ApiKey}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Get the product
+                WeatherInformation weatherInformation = await GetWeatherInfromationAsync(client.BaseAddress);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        async Task<WeatherInformation> GetWeatherInfromationAsync(Uri baseAddress)
+        {
+            WeatherInformation weatherInfromation = null;
+            HttpResponseMessage response = await client.GetAsync(baseAddress);
+            if (response.IsSuccessStatusCode)
+            {
+                weatherInfromation = await response.Content.ReadAsAsync<WeatherInformation>();
+            }
+            return weatherInfromation;
         }
     }
 }
