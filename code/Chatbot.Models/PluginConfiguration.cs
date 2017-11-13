99bb63398aceff5f7713fccc8b4550b134e65f24
Michael Foidl <if15b009@technikum-wien.at>
2017-11-13 15:48:21 +0100

    worked on #4 added PluginConfiguration model

---
+++ code/Chatbot.Models/PluginConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.Models
+{
+    public class PluginConfiguration
+    {
+        public Dictionary<string, string> Dictionary { get; set; }
+    }
+}
