68f90af140222337b980ec1c0b130fe2f29f93c7
Consti <if15b020@technikum-wien.at>
2017-11-25 16:10:20 +0100

    testing audio recording in JS

--- code/Chatbot/Scripts/messenger.js
+++ code/Chatbot/Scripts/messenger.js
@@ -88,3 +88,34 @@ function ensureSession() {
         addErrorMessage("Sorry! No Web Storage support.", true);
     }
 }
+
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        rec = new MediaRecorder(stream);
+        rec.ondataavailable = e => {
+            audioChunks.push(e.data);
+            if (rec.state == "inactive") {
+                let blob = new Blob(audioChunks, { type: 'audio/x-mpeg-3' });
+                recordedAudio.src = URL.createObjectURL(blob);
+                recordedAudio.controls = true;
+                recordedAudio.autoplay = true;
+                audioDownload.href = recordedAudio.src;
+                audioDownload.download = 'mp3';
+                audioDownload.innerHTML = 'download';
+            }
+        }
+    })
+    .catch(e => console.log(e));
+
+startRecord.onclick = e => {
+    startRecord.disabled = true;
+    stopRecord.disabled = false;
+    audioChunks = [];
+    rec.start();
+}
+stopRecord.onclick = e => {
+    startRecord.disabled = false;
+    stopRecord.disabled = true;
+    rec.stop();
+}
