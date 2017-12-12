462de39fcf4d22f0d557a7667f1bb38f8cde2f75
Michael Palata <if15b037@technikum-wien.at>
2017-12-13 00:13:06 +0100

    added order caching and submitting

--- code/Chatbot.Plugins.DeliveryServicePlugin/DeliveryService.cs
+++ code/Chatbot.Plugins.DeliveryServicePlugin/DeliveryService.cs
@@ -5,12 +5,14 @@ using Chatbot.Plugins.DeliveryServicePlugin;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Runtime.Caching;
+using System;
 
 namespace Chatbot.Plugins.DeliveryService
 {
     public class DeliveryService : IPlugin
     {
-        private static readonly string HANDLE_PATTERN = @"^.*(?:order something|please order|want to order|delivery service plugin).*$";
+        private static readonly string HANDLE_PATTERN = @"^.*(?:order something|please order|want to order|delivery service plugin|submit order).*$";
         private static readonly Regex HANDLE_REGEX = new Regex(HANDLE_PATTERN, RegexOptions.IgnoreCase);
 
         private static readonly string EXTRACT_ORDER_PATTERN = @"^[a-zA-Z\s]*:\s*((?:\d+[a-zA-ZäöüÄÖÜ\s]+(?:\([a-zA-ZäöüÄÖÜ\s]*\))?,\s*)+)\s*to:\s*([a-zA-ZäöüÄÖÜß\-\s]+,[a-zA-Z0-9äöüÄÖÜß\-\s]+,[a-zA-ZäöüÄÖÜ0-9ß\-\s]+)$";
@@ -43,6 +45,23 @@ namespace Chatbot.Plugins.DeliveryService
             if (message.Content.Contains("delivery service plugin"))
             {
                 response.Content = "Usage of the Delivery Service Plugin:\n\n[...] order: [amount] [product] [|(extras)], [amount] [product] [|(extras)], ..., to: [name], [street], [zip city]";
+                return response;
+            }
+
+            Order order;
+            ObjectCache cache = MemoryCache.Default;
+
+            if ("submit order".Equals(message.Content))
+            {
+                order = (Order) cache[message.SessionKey.ToString()];
+                if (order == null)
+                {
+                    response.Content = "No order found or order expired!";
+                    return response;
+                }
+                DeliveryServiceRepository dal = new DeliveryServiceRepository(new DeliveryServiceContext());
+                dal.Create(order);
+                response.Content = "Your order was successfully submitted!";
                 return response;
             }
 
@@ -84,14 +103,15 @@ namespace Chatbot.Plugins.DeliveryService
                 orderWrapper.Positions.Add(orderPosition);
             }
 
-            Order order = new Order();
+            order = new Order();
+            order.SessionKey = message.SessionKey.Key;
             order.SetOrder(orderWrapper);
-            
-            DeliveryServiceRepository dal = new DeliveryServiceRepository(new DeliveryServiceContext());
 
-            //dal.Create(entity);
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(2.0);
+            cache.Set(message.SessionKey.ToString(), order, policy);
             
-            response.Content = "DeliveryService Plugin TRIGGERED!\n*autistic screeching*REEEEEEEEEEEE!!!!\n\n++++++++++++++++++++++++++++++\n\n" + order.OrderJsonData;
+            response.Content = "DeliveryService Plugin TRIGGERED!\n*autistic screeching*REEEEEEEEEEEE!!!!\n\n++++++++++++++++++++++++++++++\n\n" + order.OrderJsonData + "\n\nTo submit your order, please type \"submit order\"!";
             return response;
         }
     }
